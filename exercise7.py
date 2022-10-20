48a47519bf6bd476bccfa29fda2656c9b308aaa6
“Aimee <aimee.brett@avanade.com>
2022-10-20 15:01:21 +0100

    Exercise 7 and 8 - 8 to be reviewed

---
+++ exercise7.py
@@ -0,0 +1,6 @@
+a = 1
+for a in range(1,11):
+    if a % 2 ==0:
+        print(a, "is even", ) 
+    else:
+        print(a, "is odd", ) 
---
+++ exercise8.py
@@ -0,0 +1,10 @@
+# Weird answer but looks like the flowchart?
+a = 1
+b = 1
+
+while a <= 10:
+    if b <= 10:
+        print(a)
+        b = b + 1
+    else:
+        a = a + 1
